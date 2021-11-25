47803dd6e3227c536548f47bff4df2cac41d1305
meyhoo <1302534151@qq.com>
2021-11-25 18:11:41 +0800

    111

--- src/main/java/com/shrb/versionowner/VersionownerApplication.java
+++ src/main/java/com/shrb/versionowner/VersionownerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+//aaa
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @ComponentScan(basePackages={
